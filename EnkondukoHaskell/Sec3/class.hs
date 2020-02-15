0153879835950fc1534fdbc4e0f10d19377a1f00
Dora F <doradora.prog@gmail.com>
2020-02-15 23:36:00 +0900

    edit class.hs

--- EnkondukoHaskell/Sec3/class.hs
+++ EnkondukoHaskell/Sec3/class.hs
@@ -9,10 +9,8 @@ class Greeting a where
     bye :: a -> String 
     bye _ = "..."
 
-instance Greeting Human where
-    name (Human n) = n
-    hello h = "Hi, I'm " ++ name h ++ "."
-    bye _ = "See you."
+class Greeting a => Laughing a where -- Greeting の child class
+    laugh :: a -> String
 
 instance Greeting Dog where
     name _ = "a dog"
@@ -22,10 +20,26 @@ instance Greeting Cat where
     name _ = "a cat"
     hello _ = "Meow..."
 
+instance Greeting Human where
+    name (Human n) = n
+    hello h = "Hi, I'm " ++ name h ++ "."
+    bye _ = "See you."
+
+instance Laughing Human where
+    laugh _ = "Hahaha"
+
+
+
+sayHello :: Greeting a => a -> IO ()
+sayHello x = putStrLn $ hello x
+
 main :: IO ()
 main = do
-        putStrLn $ hello Dog
-        putStrLn $ hello Cat 
-        putStrLn $ hello (Human "Takeshi")
-
+        sayHello Dog
+        sayHello Cat 
+        sayHello (Human "Takeshi")
+        putStrLn $ laugh (Human "Takeshi")
+        putStrLn $ bye Dog
+        putStrLn $ bye Cat 
+        putStrLn $ bye (Human "Takeshi")
 
